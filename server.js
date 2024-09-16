e603ffc89c85ab5e01003f64dfe30c98d277f29a
Akanksha-Singla <92256793+Akanksha-Singla@users.noreply.github.com>
2024-09-16 19:08:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`server is running at port ${PORT}`)
+})
+
+app.use("/",(req,res)=>{
+  res.send("server has started running successfully")
+})
