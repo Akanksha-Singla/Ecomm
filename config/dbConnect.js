ca217c16515de95011e49476ba0678dbcaaca732
Akanksha-Singla <92256793+Akanksha-Singla@users.noreply.github.com>
2024-09-20 11:57:57 +0530

    database connected

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,5 +1,13 @@
-const {default:mongoose} = require('mongoose')
 
-// const dbConnect ={
-//     const conn =
-// }
+
+// const EmployeeModel = require("../model/api/emplooyeeModel")
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://singlaakanksha92:3N2yyQiZi1aEVDcz@cluster0.rgxmtwz.mongodb.net/Ecomm?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then((message) => {console.log("message") 
+    // require("../model/api/employeeModel")
+    })
+
+  .catch((err) => console.log(err));
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
@@ -8,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, ()=>{
     console.log(`server is running at port ${PORT}`)
 })
-
+require("./config/dbConnect");
 app.use("/",(req,res)=>{
   res.send("server has started running successfully")
 })
